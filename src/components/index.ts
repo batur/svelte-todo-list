923ca985fac80f960d35f1772a41600baa85295a
H. Batur Akkurt <30292080+batur@users.noreply.github.com>
2023-01-14 10:40:18 +0300

    feat: form (#6)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Footer } from './Footer/index.svelte';
+export { default as Form } from './Form/index.svelte';
 export { default as Header } from './Header/index.svelte';
 export * from './Icons/index';
